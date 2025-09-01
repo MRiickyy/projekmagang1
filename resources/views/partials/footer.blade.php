687ccf26263497ec7788e7017bfa79d13776ba75
felianadiafikarda <felianf12@gmail.com>
2025-09-01 15:12:13 +0700

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,20 +1,23 @@
 <!-- FOOTER -->
 <footer class="mt-10 bg-gradient-to-br from-[#0f172a] to-[#1f2937]">
-    <div class="max-w-7xl mx-auto px-5 py-10 grid md:grid-cols-3 gap-6 text-white">
-        <div class="rounded-xl bg-slate-900/40 ring-1 ring-white/10 p-6 text-center">
-            <div class="font-extrabold mb-2">ICoICT 2025 Organized By :</div>
-            <div class="text-slate-200">Telkom University Indonesia</div>
-            <div class="mt-2 text-slate-300 text-sm">Co – Hosts :<br />Multimedia University Malaysia</div>
+    <div class="max-w-7xl mx-auto px-5 py-10 grid md:grid-cols-3 gap-6">
+        <div class="rounded-xl bg-[#F2F6F9] ring-1 ring-white/10 p-6 text-center">
+            <div class="text-slate-700 font-extrabold mb-2">ICoICT 2025 Organized By :</div>
+            <div class="text-slate-700">Telkom University Indonesia</div>
+            <img src="{{ asset('images/logoTelkom.png') }}" class="h-8 md:h-8 mx-auto mt-4" alt="logoTelkom">
+            <div class="mt-4 text-slate-700 text-sm">Co – Hosts :<br />Multimedia University Malaysia</div>
+            <img src="{{ asset('images/logoMMU.png') }}" class="h-6 md:h-8 mx-auto mt-4" alt="logoMMU">
         </div>
 
-        <div class="rounded-xl bg-slate-900/40 ring-1 ring-white/10 p-6 text-center">
-            <div class="font-extrabold mb-2">Sponsored By :</div>
-            <div class="text-slate-200">IEEE Indonesia Section</div>
+        <div class="rounded-xl bg-[#F2F6F9] ring-1 ring-white/10 p-6 text-center">
+            <div class="text-slate-700 font-extrabold mb-2">Sponsored By :</div>
+            <div class="text-slate-700">IEEE Indonesia Section</div>
+            <img src="{{ asset('images/logoIEEE.png') }}" class="h-8 md:h-12 mx-auto" alt="logoIEEE">
         </div>
 
-        <div class="rounded-xl bg-slate-900/40 ring-1 ring-white/10 p-6 text-center">
-            <div class="font-extrabold mb-2">Visitors</div>
-            <div class="text-slate-300 text-sm">[Flag counter / analytics placeholder]</div>
+        <div class="rounded-xl bg-[#F2F6F9] ring-1 ring-white/10 p-6 text-center">
+            <div class="text-slate-700 font-extrabold mb-2">Visitors</div>
+            <img src="{{ asset('images/benderaVisitor.png') }}" class="h-25 md:h-25 mx-auto" alt="benderaVisitor">
         </div>
     </div>
 </footer>
