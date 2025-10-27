ebd60fb2a97d8dcf1ae54a18e90e0afe6f2baf3b
padiw123 <fadiyaghifari200@gmail.com>
2025-10-27 17:50:51 +0700

    perbaikan header

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Event;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        View::composer('*', function ($view) {
+            $event = null;
+            if (session()->has('selected_event_id')) {
+                $event = Event::find(session('selected_event_id'));
+            }
+            $view->with('event', $event);
+        });
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
             <!-- Logo -->
             <a href="{{ route('home', ['event_year' => $eventYear]) }}"
                 class="text-2xl font-extrabold tracking-wide bg-gradient-to-r from-[#00e676] via-[#1dd1a1] to-[#38bdf8] bg-clip-text text-transparent">
-                SAIN 2025
+                {{ $event->name ?? 'Event' }} {{ $event->year ?? '' }}
             </a>
 
             <!-- Menu -->
