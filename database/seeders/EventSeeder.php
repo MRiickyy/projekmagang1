1a6cb6858bbe604531cf78eacbc5f237ec225415
padiw123 <fadiyaghifari200@gmail.com>
2025-10-28 21:23:11 +0700

    perbarui header dan database event

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -14,10 +14,22 @@ class EventSeeder extends Seeder
         $event = Event::firstOrCreate([
             'name' => 'ICOICT',
             'year' => 2025,
+            'main_title' => 'THE 13TH ICOICT',
+            'subtitle' => 'International Conference on Information and Communication Technology',
+            'location' => 'Bandung (Hybrid)',
+            'date_range' => '30–31 July 2025',
+            'register_link' => '/newacc',
+            'submit_link' => '/login',
         ]);
         $event = Event::firstOrCreate([
             'name' => 'ICOICT',
             'year' => 2024,
+            'main_title' => 'THE 12TH ICOICT',
+            'subtitle' => 'International Conference on Information and Communication Technology',
+            'location' => 'Bali (Hybrid)',
+            'date_range' => '25–26 July 2024',
+            'register_link' => '/newacc',
+            'submit_link' => '/login',
         ]);
     }
 }
