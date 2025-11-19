8d613464eb7e2c555ce6c1296071cda69cee052a
felianadiafikarda <felianf12@gmail.com>
2025-11-19 22:21:50 +0700

    admin harus login, jadi tidak bisa akses via link route done

---
+++ app/Http/Middleware/AdminAuth.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminAuth
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!session()->has('admin_logged_in') || session('admin_logged_in') !== true) {
+            return redirect()->route('admin.login')->with('error', 'Silakan login terlebih dahulu.');
+        }
+
+        return $next($request);
+    }
+
+}
